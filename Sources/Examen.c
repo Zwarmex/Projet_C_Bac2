be7aa07cb033b4528c63fb5aeabfe9a2b2196fdf
Samuel Lambert <sam.f.lambert@gmail.com>
2022-09-22 11:17:17 -0400

    From VS

--- Sources/Examen.c
+++ Sources/Examen.c
@@ -6,40 +6,61 @@
 #define LENGTHARRAY(array) (sizeof(array)/sizeof(*array))
 #define NUMBEROFCARS 20
 
-
-// Test for git
-
 struct Car
 {
+
 	int id;
 	int bestTimeCircuitMS[3];
 	int timeCircuitMS[3];
 	int turnTimeMS;
 	int position; // For later
+	int state; 
 };
 
 int main()
 {
-	int upperTimeMaxMS = 45000, lowerTimeMinMS = 25000, arrayCarsId[NUMBEROFCARS] = {44, 63, 1, 11, 55, 16, 4, 3, 14, 31, 10, 22, 5, 18, 6, 23, 77, 24, 47, 9};
+
+	int upperTimeMaxMS = 45000, lowerTimeMinMS = 25000, arrayCarsId[NUMBEROFCARS] = {44, 63, 1, 11, 55, 16, 4, 3, 14, 31, 10, 22, 5, 18, 6, 23, 77, 24, 47, 9},
+	boolQualification = 0;
 
 	srand(time(NULL));
 	
 	struct Car arrayCars[] = {CarBuilder(arrayCarsId)};
 
-	for (int i = 0; i < LENGTHARRAY(arrayCars[0].timeCircuitMS); ++i)
+	if (!boolQualification)
 	{
-		arrayCars[0].timeCircuitMS[i] = (rand() % (upperTimeMaxMS - lowerTimeMinMS + 1)) + lowerTimeMinMS;
-		printf("Temps Random S%d: %d milliseconds (%d seconds)\n", i+1, arrayCars[0].timeCircuitMS[i], MillisecondsToSeconds(arrayCars[0].timeCircuitMS[i]));
-		
-		if (arrayCars[0].bestTimeCircuitMS[i] == 0 || arrayCars[0].bestTimeCircuitMS[i] > arrayCars[0].timeCircuitMS[i])
-		{
-			arrayCars[0].bestTimeCircuitMS[i] = arrayCars[0].timeCircuitMS[i];
-		}
 
-		arrayCars[0].turnTimeMS += arrayCars[0].timeCircuitMS[i];
+		int boolContinueTesting = 1;
+		while (boolContinueTesting)
+		{
+			printf("La voiture test le circuit.\n");
+			arrayCars[0].turnTimeMS = 0;
+			for (int i = 0; i < LENGTHARRAY(arrayCars[0].timeCircuitMS); ++i)
+			{
+
+				arrayCars[0].timeCircuitMS[i] = (rand() % (upperTimeMaxMS - lowerTimeMinMS + 1)) + lowerTimeMinMS;
+				printf("Temps S%d: %d milliseconds (%d seconds)\n", i+1, arrayCars[0].timeCircuitMS[i], MillisecondsToSeconds(arrayCars[0].timeCircuitMS[i]));
+				
+				if (arrayCars[0].bestTimeCircuitMS[i] == 0 || arrayCars[0].bestTimeCircuitMS[i] > arrayCars[0].timeCircuitMS[i])
+				{
+
+					arrayCars[0].bestTimeCircuitMS[i] = arrayCars[0].timeCircuitMS[i];
+				}
+
+				arrayCars[0].turnTimeMS += arrayCars[0].timeCircuitMS[i];
+			}
+			PrintCarTurnTiming(arrayCars[0]);
+			boolContinueTesting =  rand() % 2;
+		}
 	}
 
-	PrintCarTurnTiming(arrayCars[0]);
+	else
+	{
+		
+	}
+	
+	
+	
 
 	return 0;
 }
@@ -47,23 +68,23 @@ int main()
 void PrintCarTurnTiming(struct Car car)
 {
 	int *bestTimeExpr = BestTimeFromMS(car.turnTimeMS);
-	printf("Print : %d,      %d\n", bestTimeExpr[0], bestTimeExpr[1]);
 	switch(bestTimeExpr[0])
 	{
+
 		case 1:
-			printf("Car 1 : %d\nTime for the whole turn : %d seconds.\n", car.id, bestTimeExpr[1]);
+			printf("Car %d : Time for the whole turn : %d seconds.\n", car.id, bestTimeExpr[1]);
 		break;
 
 		case 2:
-			printf("Car 1 : %d\nTime for the whole turn : %d minutes.\n", car.id, bestTimeExpr[1]);
+			printf("Car %d : Time for the whole turn : %d minutes.\n", car.id, bestTimeExpr[1]);
 		break;
 
 		case 3:
-			printf("Car 1 : %d\nTime for the whole turn : %d hours.\n", car.id, bestTimeExpr[1]);
+			printf("Car %d : Time for the whole turn : %d hours.\n", car.id, bestTimeExpr[1]);
 		break;
 
 		default:
-			printf("Car 1 : %d\nTime for the whole turn : %d milliseconds.\n", car.id, car.turnTimeMS);
+			printf("Car %d : Time for the whole turn : %d milliseconds.\n", car.id, car.turnTimeMS);
 	}
 
 }
@@ -73,21 +94,21 @@ int* BestTimeFromMS(int ms)
 	int static bestTime[2] = {0, 0};
 	if (ms > 2000)
 	{
+
 		bestTime[0] = 1;
 		bestTime[1] = MillisecondsToSeconds(ms);
-		printf("%d\n", bestTime[1]);
 	}
 	if (bestTime[1] > 120)
 	{
+
 		bestTime[0] = 2;
 		bestTime[1] = SecondsToMinutes(bestTime[1]);
-		printf("%d\n", bestTime[1]);
 	}
 	if (bestTime[1] > 120)
 	{
+
 		bestTime[0] = 3;
 		bestTime[1] = MinutesToHours(bestTime[1]);
-		printf("%d\n", bestTime[1]);
 	}
 	
 	return bestTime;
@@ -95,28 +116,34 @@ int* BestTimeFromMS(int ms)
 
 int MillisecondsToSeconds(int ms)
 {
+
 	return ms/1000;
 }
 
 int SecondsToMinutes(int secs)
 {
+
 	return secs/60;
 }
 
 int MinutesToHours(int min)
 {
+
 	return	min/60;
 }
 
 struct Car CarBuilder(int arrayId[])
 {
+
 	struct Car arrayCars[NUMBEROFCARS];
 
 	for (int i = 0; i < NUMBEROFCARS; i++)
 	{
+
 		arrayCars[i].id = arrayId[i];
 		// arrayCars[i].bestTimeCircuitMS;
 		arrayCars[i].turnTimeMS = 0;
+		arrayCars[i].state = 0; // Ready to starting
 		// printf("Car built n° %d :\nCar number : %d\n\n", i, arrayCars[i].id);
 	}
 
