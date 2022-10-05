7da44d5b34bbedfbe36d60f643f888edbfa9cd3d
Samuel Lambert <sa.lambert@gmail.com>
2022-10-05 16:48:16 -0400

    sh m works great

--- Examen.c
+++ Examen.c
@@ -14,10 +14,10 @@ int main() // Add boolClassicWeekEnd in arg
 {
 
 	// Initialisation of variables
-	int boolSprint = 0, boolClassicWeekEnd = 1, shmSize = sizeof(int) * NUMBEROFCARS, shmData[NUMBEROFCARS], shmId,
+	int boolSprint = 0, boolClassicWeekEnd = 1, shmSize = sizeof(Car) * NUMBEROFCARS, shmData[NUMBEROFCARS], shmId,
 	arrayCarsId[NUMBEROFCARS] = {44, 63, 1, 11, 55};//, 16, 4, 3, 14, 31, 10, 22, 5, 18, 6, 23, 77, 24, 47, 9};
 
-	int *shMem;
+	Car *shMem;
 
 	if((shmId = shmget(IPC_PRIVATE, shmSize, IPC_CREAT | 0775)) < 0)
 	{
@@ -25,7 +25,7 @@ int main() // Add boolClassicWeekEnd in arg
 		exit(EXIT_FAILURE);
 	} 
 	
-	if ((shMem = (int *) shmat(shmId, NULL, 0)) < 0)
+	if ((shMem = (Car *) shmat(shmId, NULL, 0)) < 0)
 	{
 		perror("shmat error ");
 		exit(EXIT_FAILURE);
@@ -60,7 +60,7 @@ int main() // Add boolClassicWeekEnd in arg
 				DoFreeTry(&arrayCars[i]);
 				printf("\nSaturday's morning : Free Try\n\n");
 				DoFreeTry(&arrayCars[i]);
-				memcpy(&shMem[i], &arrayCars[i].totalTurnMS, sizeof(arrayCars[i].totalTurnMS));
+				memcpy(&shMem[i], &arrayCars[i], sizeof(arrayCars[i]));
 			}	
 			// Child have to not make another child
 			exit(EXIT_SUCCESS);
@@ -73,21 +73,13 @@ int main() // Add boolClassicWeekEnd in arg
 	int waitRespons, waitStatus = 0;
 	while ((waitRespons = wait(&waitStatus)) > 0);
 
-	for (int i = 0; i < NUMBEROFCARS; i++)
-	{
-		int *data = (int *) (&shMem[i]);
-		printf("\n\nshm : %d\n", *data);
-	}
-
-	exit(EXIT_SUCCESS);
-	
 	FILE *pointerFileScore = fopen("Results/score.txt", "w");
 
 	for (int i = 0; i < NUMBEROFCARS; i++)
 	{
-		fprintf(pointerFileScore, "Car : %d -> Turn : %d\n", arrayCars[i].id, arrayCars[i].turnTimeMS);
+		Car *car = (Car *) (&shMem[i]);
+		fprintf(pointerFileScore, "Car : %d -> Turn : %d\n", car->id, car->turnTimeMS);
 	}
-	
 
 	fclose(pointerFileScore);
 		
