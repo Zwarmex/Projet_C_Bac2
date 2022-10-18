93bf95ff0a77bbd79e2853c2f10018ce149151f6
Samuel Lambert <sa.lambert@gmail.com>
2022-10-18 07:54:16 -0400

    uygug

--- Sources/FunctionsCars.c
+++ Sources/FunctionsCars.c
@@ -75,7 +75,7 @@ void DoRace(Car *car, int minutes, Car *shMem)
 			// Car time is randomize
 			car->timeSectionMS[i] = RandomNumber(lowerTimeMinMS, upperTimeMaxMS);
 			
-			// sleep(car->timeSectionMS[i]);
+			// sleep(car->timeSectionMS[i]/10);
 
 			// if the car is doing a better time 
 			if (!car->bestTimeSectionMS[i]|| car->bestTimeSectionMS[i] > car->timeSectionMS[i])
