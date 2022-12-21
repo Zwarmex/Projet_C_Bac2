4c9fb2bb0b166f8925a9299de61827699b276089
Samuel Lambert <92597536+Samaze123@users.noreply.github.com>
2022-12-21 03:34:11 -0500

    Delete Test directory

--- Test/cursor.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("\033[47m\033[30mTest1  Test2\n\033[39m\033[49m");
-    return 0;
-}
